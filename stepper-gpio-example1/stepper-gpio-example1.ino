22b767d735013110705c724d4e029a282f2942d7
Kunal <kunalguptakew@gmail.com>
2020-10-23 04:07:42 +0530

    adding  refrence for AVR

---
+++ stepper-gpio-example1/stepper-gpio-example1.ino
@@ -0,0 +1,121 @@
+//original source is http://www.geeetech.com/wiki/index.php/Stepper_Motor_5V_4-Phase_5-Wire_%26_ULN2003_Driver_Board_for_Arduino
+// Update by Ahmad Shamshiri for RoboJax.com
+// Published on March 27, 2017 in Aajx, ON, Canada.
+/*
+ * 
+  Need wiring diagram from this code:  https://youtu.be/0glBk917HPg
+  Purchase My Arduino course on Udemy.com http://robojax.com/L/?id=62
+ * 
+
+ * Get this code and other Arduino codes from Robojax.com
+Learn Arduino step by step in structured course with all material, wiring diagram and library
+all in once place. Purchase My course on Udemy.com http://robojax.com/L/?id=62
+
+If you found this tutorial helpful, please support me so I can continue creating 
+content like this. You can support me on Patreon http://robojax.com/L/?id=63
+
+or make donation using PayPal http://robojax.com/L/?id=64
+
+ *  * This code is "AS IS" without warranty or liability. Free to be used as long as you keep this note intact.* 
+ * This code has been download from Robojax.com
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see .
+ */
+
+
+int Pin1 = 8; 
+int Pin2 = 9; 
+int Pin3 = 10; 
+int Pin4 = 11; 
+int _step = 0; 
+boolean dir = false;// false=clockwise, true=counter clockwise
+int count=0;
+void setup() 
+{ 
+ pinMode(Pin1, OUTPUT);  
+ pinMode(Pin2, OUTPUT);  
+ pinMode(Pin3, OUTPUT);  
+ pinMode(Pin4, OUTPUT);  
+} 
+ void loop() 
+{ 
+ switch(_step){ 
+   case 0: 
+     digitalWrite(Pin1, LOW);  
+     digitalWrite(Pin2, LOW); 
+     digitalWrite(Pin3, LOW); 
+     digitalWrite(Pin4, HIGH); 
+   break;  
+   case 1: 
+     digitalWrite(Pin1, LOW);  
+     digitalWrite(Pin2, LOW); 
+     digitalWrite(Pin3, HIGH); 
+     digitalWrite(Pin4, HIGH); 
+   break;  
+   case 2: 
+     digitalWrite(Pin1, LOW);  
+     digitalWrite(Pin2, LOW); 
+     digitalWrite(Pin3, HIGH); 
+     digitalWrite(Pin4, LOW); 
+   break;  
+   case 3: 
+     digitalWrite(Pin1, LOW);  
+     digitalWrite(Pin2, HIGH); 
+     digitalWrite(Pin3, HIGH); 
+     digitalWrite(Pin4, LOW); 
+   break;  
+   case 4: 
+     digitalWrite(Pin1, LOW);  
+     digitalWrite(Pin2, HIGH); 
+     digitalWrite(Pin3, LOW); 
+     digitalWrite(Pin4, LOW); 
+   break;  
+   case 5: 
+     digitalWrite(Pin1, HIGH);  
+     digitalWrite(Pin2, HIGH); 
+     digitalWrite(Pin3, LOW); 
+     digitalWrite(Pin4, LOW); 
+   break;  
+     case 6: 
+     digitalWrite(Pin1, HIGH);  
+     digitalWrite(Pin2, LOW); 
+     digitalWrite(Pin3, LOW); 
+     digitalWrite(Pin4, LOW); 
+   break;  
+   case 7: 
+     digitalWrite(Pin1, HIGH);  
+     digitalWrite(Pin2, LOW); 
+     digitalWrite(Pin3, LOW); 
+     digitalWrite(Pin4, HIGH); 
+   break;  
+   default: 
+     digitalWrite(Pin1, LOW);  
+     digitalWrite(Pin2, LOW); 
+     digitalWrite(Pin3, LOW); 
+     digitalWrite(Pin4, LOW); 
+   break;  
+ } 
+ if(dir){ 
+   _step++; 
+ }else{ 
+   _step--; 
+ } 
+ if(_step>7){ 
+   _step=0; 
+ } 
+ if(_step<0){ 
+   _step=7; 
+ } 
+ delay(1); 
+
+}
